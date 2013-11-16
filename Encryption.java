89ab9449e9590e8f0c4a98c85d03fe8d622b3ec2
Kuo Hern Wong <Kuohern@My-Internet.local>
2013-11-16 18:21:41 +0000

    added hash function.

--- Encryption.java
+++ Encryption.java
@@ -2,6 +2,7 @@ package com.example.musoni;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
@@ -17,8 +18,15 @@ import javax.crypto.spec.SecretKeySpec;
 public class Encryption {
 	
 	static String algorithm = "AES";
+	static String hashAlg = "SHA-256";
 	
-	private static byte[] generateKey(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException {
+	public static String hash(String password) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance(hashAlg);
+		return new String(digest.digest(password.getBytes("UTF-8")));
+		
+	}
+	
+	public static byte[] generateKey(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException {
 		byte[] keyStart = password.getBytes("UTF-8");
 		KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
 		SecureRandom scr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
@@ -28,16 +36,14 @@ public class Encryption {
 		return sKey.getEncoded();
 	}
 	
-	public static byte[] encrypt(String password, byte[] data) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-		byte[] key = generateKey(password);
+	public static byte[] encrypt(byte[] key, byte[] data) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 		SecretKeySpec sKeySpec = new SecretKeySpec(key, algorithm);
 		Cipher cipher = Cipher.getInstance(algorithm);
 		cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
 		return cipher.doFinal(data);
 	}
 	
-	public static byte[] decode(String password, byte[] encrypted) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		byte[] key = generateKey(password);
+	public static byte[] decode(byte[] key, byte[] encrypted) throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		SecretKeySpec sKeySpec = new SecretKeySpec(key, algorithm);
 		Cipher cipher = Cipher.getInstance(algorithm);
 		cipher.init(Cipher.DECRYPT_MODE, sKeySpec);
