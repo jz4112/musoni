361c0492c5f433314d5d3781e94b3730a846acfe
bleed310 <paul@blid.me>
2013-11-17 01:01:11 +0000

    Main HTML & JavaScript, tested Locally, version 1

--- html/includes/interact.js
+++ html/includes/interact.js
@@ -1,3 +1,21 @@
-window.Interact = Object();
-window.Interact.canAnimate = function() { return true; };
-window.Interact.attemptLogIn = function(user, pass) { alert(user + pass); };
+/* hack to detect if we're inside android
+   if we are, do not load our dummy object, the real thing is already present!
+   */
+
+var ua = navigator.userAgent.toLowerCase();
+if(ua != null && ua.indexOf("android") == -1)
+{
+	window.Interact = Object();
+	window.Interact.canAnimate = function() { return false; };
+	window.Interact.attemptLogIn = function(user, pass) { return prompt("Enter return value for attemptLogIn(" + user + ", " + pass + "):", ""); };
+	window.Interact.navigateToMain = function() { alert("navigateToMain called."); };
+	
+	// main
+	window.Interact.navigateToRegistration = function() { alert("navigateToRegistration called."); };
+	window.Interact.navigateToLoan = function() { alert("navigateToLoan called."); };
+	window.Interact.navigateToBusiness = function() { alert("navigateToBusiness called."); };
+	window.Interact.isSyncPending = function() { return confirm("Return true on SyncPending?"); };
+	window.Interact.getCurrentUsername = function() { return prompt("Return value for getCurrentUsername", "testuser"); };
+
+}
+
