019fdbeb3863709cd91e85ce52263b2aadb7c239
Jeremy Kong <jeremykong@hotmail.com>
2013-11-17 00:12:41 +0000

    LocalStore done.

--- src/localStore/LocalStore.java
+++ src/localStore/LocalStore.java
@@ -2,7 +2,6 @@ package localStore;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,20 +20,13 @@ import forms.Form;
 
 public class LocalStore implements Serializable {
 	private static final long serialVersionUID = 42L;
-	private static String dirPathName = "." + File.separator + "form lists";
-	private static File dir;
 	private static List<Form> forms;
 	private static LocalStore instance = null;
 	private static Context context;
 
 	private LocalStore(Context context) {
 		forms = new LinkedList<Form>();
-		dir = new File(dirPathName);
-		do {
-			dir.mkdirs();
-		} while (!dir.isDirectory());
-
-		this.context = context;
+		LocalStore.context = context;
 	}
 
 	public static void setUp(Context context) {
@@ -69,15 +61,13 @@ public class LocalStore implements Serializable {
 
 		forms.add(newForm);
 		// create a new file
-		File outputFile = new File(dir, filePath(newForm));
-
 		OutputStream buffer;
 
-		buffer = new BufferedOutputStream(new FileOutputStream(outputFile));
-
+		FileOutputStream fos = context.openFileOutput(filePath(newForm), Context.MODE_PRIVATE);
+		buffer = new BufferedOutputStream(fos);
 		ObjectOutput output = new ObjectOutputStream(buffer);
 		output.writeObject(newForm);
-
+		output.flush();
 		output.close();
 	}
 
@@ -89,8 +79,9 @@ public class LocalStore implements Serializable {
 	 */
 	public boolean loadAll() throws ClassNotFoundException, IOException {
 		InputStream buffer;
-		for (File f : dir.listFiles()) {
-			buffer = new BufferedInputStream(new FileInputStream(f));
+		for (String fileName : context.fileList()) {
+		  FileInputStream fis = context.openFileInput(fileName);
+			buffer = new BufferedInputStream(fis);
 			ObjectInput in = new ObjectInputStream(buffer);
 			Form form = (Form)in.readObject();
 			forms.add(form);
