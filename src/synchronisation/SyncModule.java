cecd5775abd57d390dffa81715b496aebf253f24
Jeremy Kong <jeremykong@hotmail.com>
2013-11-17 00:00:27 +0000

    Pushing formsToSync()

--- src/synchronisation/SyncModule.java
+++ src/synchronisation/SyncModule.java
@@ -1,26 +1,51 @@
 package synchronisation;
 
+import java.util.List;
+
+import localStore.LocalStore;
 import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import forms.ClientRegistrationForm;
 import forms.Form;
 
 public class SyncModule {
 
+  private static String httpsURL = "";
+
   public static boolean WifiConnected() {
-    ConnectivityManager connManager = (ConnectivityManager) (new Activity()).getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    ConnectivityManager connManager = (ConnectivityManager) (new Activity())
+        .getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo mWifi = connManager
+        .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
     return mWifi.isConnected();
   }
 
-  public static void uploadForm(Form f) {
-    // TODO
+  public static boolean formsToSync() {
+    LocalStore store = LocalStore.getInstance();
+    List<Form> forms = store.getForms();
+    for(Form f : forms) {
+      if(f.toBeSynced()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void uploadForm(Form form) {
+    // TODO Create and send JSON requests to create a client and add to the data
+    // table.
   }
 
   public static void uploadAllForms() {
-    // TODO
+    LocalStore store = LocalStore.getInstance();
+    List<Form> list = store.getForms();
+
+    for (Form form : list) {
+      uploadForm(form);
+    }
   }
 
 }
